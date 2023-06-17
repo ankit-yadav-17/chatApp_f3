c6e016c4a5d48654983494bf376b4e1cba64a88d
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-17 11:33:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const socket = io();
+
+let username = "";
+
+document.getElementById("join-btn").addEventListener("click", (event) => {
+  event.preventDefault();
+  // console.log("test")
+  username = document.getElementById("username-input").value;
+  if (username.trim() != "") {
+    document.querySelector(".form-username").style.display("none");
+    document.querySelector(".chatroom-container").style.display("block");
+    document.querySelector(".chatroom-header"),
+      (innerHTML = `Chatroom - ${username}`);
+  }
+});
+
+document.getElementById("send-btn").addEventListener("click", (event) => {
+  event.preventDefault();
+  const data = {
+    username: username,
+    message: document.getElementById("message-input").value.trim(),
+  };
+  // emitting the 'message' event
+  socket.emit("message", data);
+  addMessage(data, true); //true -> sent
+});
+
+//recieving the message
+socket.on("message", (data) => {
+  if (data.username !== username) {
+    addMessage(data, false);
+  }
+});
+
+// this function is just for appending message
+function addMessage(data, check) {
+  // check --> true for sent
+  // check --> false for recieved
+  var msgDiv = documnet.createElement("div");
+  msgDiv.innerText = `${data.username}: ${data.message}`;
+  if (check) {
+    msgDiv.setAttribute("class", "message sent");
+  } else {
+    msgDiv.setAttribute("class", "message recieved");
+  }
+  document.getElementById("message-container").appendChild(msgDiv);
+  document.getElementById("message-input").value = "";
+}
